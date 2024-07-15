fc5ef0b99b6921f4556c8794460dc7cd093859ac
Bilguun <soproatlol@gmail.com>
2024-07-15 14:03:56 +0800

    Push

--- unity/Assets/Scripts/Player.cs
+++ unity/Assets/Scripts/Player.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -14,5 +12,10 @@ public class Player : MonoBehaviour
             return;
         }
         Singelton.SetPlayer(this);
+    }
+
+    public void handleRestart()
+    {
+        Debug.Log("Handle restart.......");
     }
 }
--- unity/Assets/Scripts/Respawner.cs
+++ unity/Assets/Scripts/Respawner.cs
@@ -4,7 +4,6 @@ public class Respawner : MonoBehaviour
 {
     void OnTriggerExit2D(Collider2D col)
     {
-        // Debug.Log(col.gameObject.name + " : " + gameObject.name + " : " + Time.time);
-        // Player
+        Singelton.GetPlayer().handleRestart();
     }
 }
